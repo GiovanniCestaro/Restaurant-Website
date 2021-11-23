90abe6370b8041dd6a292366f7129a6fc26b8c48
unknown <cestarogiovanni@gmail.com>
2021-11-23 19:59:19 -0300

    adicionado section footer

--- js/script.js
+++ js/script.js
@@ -6,9 +6,28 @@ menu.onclick = () => {
   navbar.classList.toggle('active')
 }
 
+let section = document.querySelectorAll('section')
+let navLinks = document.querySelectorAll('header .navbar a')
+
 window.onscroll = () => {
   menu.classList.remove('fa-times')
   navbar.classList.remove('active')
+
+  section.forEach(sec => {
+    let top = window.scrollY
+    let height = sec.offsetHeight
+    let offset = sec.offsetTop - 150
+    let id = sec.getAttribute('id')
+
+    if (top >= offset && top < offset + height) {
+      navLinks.forEach(links => {
+        links.classList.remove('active')
+        document
+          .querySelector('header .navbar a[href*=' + id + ']')
+          .classList.add('active')
+      })
+    }
+  })
 }
 
 document.querySelector('#search-icon').onclick = () => {
@@ -43,16 +62,16 @@ var swiper = new Swiper('.review-slider', {
   loop: true,
   breakpoints: {
     0: {
-      slidesPerView: 1,
+      slidesPerView: 1
     },
     640: {
-      slidesPerView: 2,
+      slidesPerView: 2
     },
     768: {
-      slidesPerView: 2,
+      slidesPerView: 2
     },
     1024: {
-      slidesPerView: 3,
-    },
-  },
+      slidesPerView: 3
+    }
+  }
 })
